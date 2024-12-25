940a6455990501d5333f2ea8734612cb2dd8dcca
mejbah99 <mejbahmorsalin521@gmail.com>
2024-12-26 02:02:40 +0600

    Js name Changed

---
+++ function.js
@@ -0,0 +1,17 @@
+function appleOrangeJuice(){
+    console.log('Make This Juice');
+}
+appleOrangeJuice();
+
+let age = 21;
+let name = ('Mejbah Morsalin');
+
+console.log(`I am ${age} years Old`);
+console.log(`I am ${name} and, I am ${age} years Old`);
+
+function mangoLimeJuice(mangos , limes){
+   const juice = `Make Juice with ${mangos} Mangos and add ${limes} Limes`;
+   return juice;
+}
+const juice = mangoLimeJuice(3, 5);
+console.log(juice)
